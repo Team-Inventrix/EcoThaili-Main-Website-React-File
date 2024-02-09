7e4b749e332fce715383341dd08049c032685fc8
Ningsang Jabegu <86409265+Ningsang-Jabegu@users.noreply.github.com>
2024-02-10 00:13:02 +0545

    .eslintric.js file uploaded to make the linter check for the proper jsx syntax, Previously it was not accepting jsx syntax properly

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+//Rules for the linter:
+module.exports = {
+    parser: 'babel-eslint',
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended'
+    ],
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true
+      }
+    },
+    env: {
+      browser: true,
+      node: true,
+      es6: true
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  };
+  
