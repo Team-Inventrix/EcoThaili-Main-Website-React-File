b9b350fc8f6e524b139ed68ab27798e82d459918
Ningsang Jabegu <86409265+Ningsang-Jabegu@users.noreply.github.com>
2024-02-16 23:49:40 +0545

    updated: slider component is getting new look

--- src/component/MainSlider.js
+++ src/component/MainSlider.js
@@ -1,41 +1,40 @@
-import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views-react-18';
-import { autoPlay } from 'react-swipeable-views-utils';
-import SliderImg1 from '../image/sliderImages/slider-1.jpg'
-import SliderImg2 from '../image/sliderImages/slider-2.jpg'
-import SliderImg3 from '../image/sliderImages/slider-3.jpg'
-import SliderImg4 from '../image/sliderImages/slider-4.jpg'
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MobileStepper from "@mui/material/MobileStepper";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import SwipeableViews from "react-swipeable-views-react-18";
+import { autoPlay } from "react-swipeable-views-utils";
+import SliderImg1 from "../image/sliderImages/slider-1.jpg";
+import SliderImg2 from "../image/sliderImages/slider-2.jpg";
+import SliderImg3 from "../image/sliderImages/slider-3.jpg";
+import SliderImg4 from "../image/sliderImages/slider-4.jpg";
+import { styled } from "@mui/system";
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews, { interval: 5000 });
 
 const images = [
   {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath:
-      SliderImg1,
+    label: "San Francisco – Oakland Bay Bridge, United States",
+    imgPath: SliderImg1,
   },
   {
-    label: 'Bird',
-    imgPath:
-      SliderImg2,
+    label: "Bird",
+    imgPath: SliderImg2,
   },
   {
-    label: 'Bali, Indonesia',
-    imgPath:
-      SliderImg3,
+    label: "Bali, Indonesia",
+    imgPath: SliderImg3,
   },
   {
-    label: 'Goč, Serbia',
-    imgPath:
-      SliderImg4,
+    label: "Goč, Serbia",
+    imgPath: SliderImg4,
   },
 ];
 
@@ -56,85 +55,116 @@ function MainSlider() {
     setActiveStep(step);
   };
 
+  const StyledContainer = styled(Container)({
+    padding: "5rem 1.5rem",
+   
+    '@media (min-width:600px)': {
+      // padding: "5rem",
+      textAlign: "center",
+    },
+  });
+
   return (
-    <Box sx={{ maxWidth: "100vw", minHeight: "100vh", position:"relative"}} id="ms">
+    <Container id="os" sx={{ py: { xs: 8, sm: 16 } }}>
+      <Grid container spacing={6}>
+        <Grid item xs={12} md={12}>
+          <div>
+            <Box
+              sx={{
+                // maxWidth: "100vw",
+                // minHeight: "100vh",
+                position: "relative",
+              }}
+              id="ms"
+            >
+              <AutoPlaySwipeableViews
+                axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+                index={activeStep}
+                onChangeIndex={handleStepChange}
+                enableMouseEvents
+              >
+                {images.map((step, index) => (
+                  <div
+                    key={step.label}
+                    style={{
+                      aspectRatio: "16/9",
+                      width: "100%",
+                      overflow: "hidden",
+                    }}
+                  >
+                    {Math.abs(activeStep - index) <= 2 ? (
+                      <Box
+                        component="img"
+                        sx={{
+                          height: "100%",
+                          width: "100%",
+                          display: "block",
+                          overflow: "hidden",
+                          // minWidth: "100%",
+                          margin: "auto",
+                          objectFit: "contain",
+                          objectPosition: "center",
+                        }}
+                        src={step.imgPath}
+                        alt={step.label}
+                      />
+                    ) : null}
+                  </div>
+                ))}
+              </AutoPlaySwipeableViews>
 
-      <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-      >
-        {images.map((step, index) => (
-          <div key={step.label} style={{height: "100vh", width:"100%", overflow: "hidden"}}>
-            {Math.abs(activeStep - index) <= 2 ? (
-              <Box
-                component="img"
-                sx={{
-                  height: "100%",
-                  display: 'block',
-                  overflow: 'hidden',
-                  minWidth: '100%',
-                  margin: "auto",
-                  objectFit: "fill",
-                  objectPosition: "center"
-                }}
-                src={step.imgPath}
-                alt={step.label}
-              />
-            ) : null}
+              {/* <Paper
+  square
+  elevation={0}
+  position="absolute"
+  sx={{
+    display: 'flex',
+    alignItems: 'center',
+    height: 50,
+    pl: 2,
+    bgcolor: 'transparant',
+  }}
+  
+>
+  <Typography style={{fontWeight:"bold"}}>{images[activeStep].label}</Typography>
+</Paper>
+
+<MobileStepper
+  steps={maxSteps}
+  position="absolute"
+  top="0"
+  left="0"
+  activeStep={activeStep}
+  nextButton={
+    <Button
+      size="small"
+      onClick={handleNext}
+      disabled={activeStep === maxSteps - 1}
+    >
+      Next
+      {theme.direction === 'rtl' ? (
+        <KeyboardArrowLeft />
+      ) : (
+        <KeyboardArrowRight />
+      )}
+    </Button>
+  }
+  backButton={
+    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+      {theme.direction === 'rtl' ? (
+        <KeyboardArrowRight />
+      ) : (
+        <KeyboardArrowLeft />
+      )}
+      Back
+    </Button>
+  }
+/> */}
+            </Box>
           </div>
-        ))}
-      </AutoPlaySwipeableViews>
-      
-      {/* <Paper
-        square
-        elevation={0}
-        position="absolute"
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          height: 50,
-          pl: 2,
-          bgcolor: 'transparant',
-        }}
-        
-      >
-        <Typography style={{fontWeight:"bold"}}>{images[activeStep].label}</Typography>
-      </Paper>
-
-      <MobileStepper
-        steps={maxSteps}
-        position="absolute"
-        top="0"
-        left="0"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            Next
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        }
-      /> */}
-    </Box>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
