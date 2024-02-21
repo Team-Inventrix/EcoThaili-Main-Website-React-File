af6bcda9035c2eb3cfb243e3646ba19e12022940
Ningsang Jabegu <86409265+Ningsang-Jabegu@users.noreply.github.com>
2024-02-21 06:21:00 +0545

    Navigation now works when, the page is out of homepage

--- src/component/HeroSection.js
+++ src/component/HeroSection.js
@@ -4,8 +4,9 @@ import Typography from "@mui/material/Typography";
 import { styled } from "@mui/system";
 import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 import HeroImg from "../image/hero-img.PNG";
-import { Button } from "react-scroll";
+import { Button as MuiButton } from "@mui/material";
 import { Box } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Title = styled(Typography)({
   color: "#226a49",
@@ -21,14 +22,18 @@ export default function HeroSection() {
       maxWidth="xl"
       sx={{
         height: { sm: "100%" },
-        minHeight: { xs:"100%",sm: "100%", md: "100vh" },
+        minHeight: { xs: "100%", sm: "100%", md: "100vh" },
         display: "flex",
-        flexDirection:{xs:"column-reverse",sm:"row"},
+        flexDirection: { xs: "column-reverse", sm: "row" },
         justifyContent: "space-between",
         alignItem: "center",
-        gap: { xs:"0",sm:"5vw", md: "5vw" },
+        gap: { xs: "0", sm: "5vw", md: "5vw" },
         width: "100%",
-        padding: { xs:"5rem 5vw 5rem 5vw",sm: "8rem 5vw 5rem 5vw", md: "10rem 5vw 5rem 5vw" },
+        padding: {
+          xs: "5rem 5vw 5rem 5vw",
+          sm: "8rem 5vw 5rem 5vw",
+          md: "10rem 5vw 5rem 5vw",
+        },
         textAlign: "center",
       }}
     >
@@ -39,7 +44,7 @@ export default function HeroSection() {
           justifyContent: "center",
           alignItem: "center",
           flexDirection: "column",
-          gap: {xs:"3vh",sm:"3vw"},
+          gap: { xs: "3vh", sm: "3vw" },
         }}
       >
         <Box>
@@ -48,14 +53,22 @@ export default function HeroSection() {
               color: "#226a49",
               textAlign: "center",
               fontWeight: 700,
-              fontSize: {xs:"clamp(3.5rem, 4vw, 4rem)",sm:"clamp(4.5rem, 6vw, 6rem)",md:"clamp(2rem, 6vw, 6.2rem)",},
+              fontSize: {
+                xs: "clamp(3.5rem, 4vw, 4rem)",
+                sm: "clamp(4.5rem, 6vw, 6rem)",
+                md: "clamp(2rem, 6vw, 6.2rem)",
+              },
             }}
           >
             EcoThaili
           </Typography>
           <Typography
             sx={{
-              fontSize: {xs:"clamp(1rem, 2vw, 2rem)",sm:"clamp(1.3rem, 1.3vw, 3rem)",md:"clamp(1.5rem, 1.3vw, 3rem)",},
+              fontSize: {
+                xs: "clamp(1rem, 2vw, 2rem)",
+                sm: "clamp(1.3rem, 1.3vw, 3rem)",
+                md: "clamp(1.5rem, 1.3vw, 3rem)",
+              },
               margin: "-1rem 0 0 0",
               fontWeight: "bold",
             }}
@@ -63,41 +76,53 @@ export default function HeroSection() {
             Bag The Future Responsibly
           </Typography>
           <Typography
-            sx={{ fontSize: {xs:"clamp(.8rem, 1vw, 1rem)",sm:"clamp(1rem, 1vw, 1.4rem)"}, opacity: ".7" }}
+            sx={{
+              fontSize: {
+                xs: "clamp(.8rem, 1vw, 1rem)",
+                sm: "clamp(1rem, 1vw, 1.4rem)",
+              },
+              opacity: ".7",
+            }}
           >
             🌿#EcoThaili #BeatPlasticWithUs
           </Typography>
         </Box>
-        <Button
-          style={{
-            width: "max-content",
-            padding: "1rem 2rem",
-            fontSize: "clamp(1rem, 1vw, 2rem)",
-            margin: "0 auto",
-            border: "none",
-            borderRadius: ".5rem",
-            color: "#fff",
-            fontWeight: "600",
-            background: "#226A49",
-            cursor: "pointer",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-          sx={{
-            "&:hover": {
-              background: "#6E8057",
-            },
-          }}
-        >
-          Get in Touch &nbsp;&nbsp;
-          <ArrowCircleRightIcon />
-        </Button>
+        <Link to="/contact">
+          <MuiButton
+            style={{
+              width: "max-content",
+              padding: "1rem 2rem",
+              fontSize: "clamp(1rem, 1vw, 2rem)",
+              margin: "0 auto",
+              border: "none",
+              borderRadius: ".5rem",
+              color: "#fff",
+              fontWeight: "600",
+              background: "#226A49",
+              cursor: "pointer",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            sx={{
+              "&:hover": {
+                background: "#6E8057",
+              },
+            }}
+          >
+            Get in Touch &nbsp;&nbsp;
+            <ArrowCircleRightIcon />
+          </MuiButton>
+        </Link>
       </Box>
       <Box
         style={{}}
         sx={{
-          width: {xs: "calc(100vw - 10vw)", sm: "calc(50vw - 10vw)", md: "calc(50vw - 10vw)",  },
+          width: {
+            xs: "calc(100vw - 10vw)",
+            sm: "calc(50vw - 10vw)",
+            md: "calc(50vw - 10vw)",
+          },
         }}
       >
         <img
@@ -105,6 +130,9 @@ export default function HeroSection() {
           alt="EcoThaili Bag Hero Image"
           height="auto"
           width="100%"
+          style={{
+            filter: "drop-shadow(0px 0px 7px rgba(0, 0, 0,.3))",
+          }}
         />
       </Box>
     </Container>
